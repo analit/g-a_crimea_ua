855f318e2de97b16dbd0fdccc1adeb49ba2b5207
sergei <analit09@mail.ru>
2013-09-28 20:44:11 +0300

    Change styles.

--- wp-content/themes/mytheme/category-ga.php
+++ wp-content/themes/mytheme/category-ga.php
@@ -1,11 +1,10 @@
-<div class="blog_content blog_page">
     <div class="sites-galary" >
         <ul>
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                     <li>
                         <a href="">
                             <span class="img_holder">
-                                <img alt="" src="wp-content/themes/mytheme/182.jpg" style="height: 232px; width: 232px">
+                                <img alt="" src="wp-content/themes/mytheme/182.jpg" style="height: 235px; width: 235px">
                                 <span class="imgTitle"><?php echo get_the_title(); ?> </span>
                             </span>
                         </a>
@@ -16,4 +15,3 @@
         </ul>
         <div class="clear"></div>
     </div>
-</div>
--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -3,7 +3,7 @@
     <head>
         <?php get_header(); ?> 
     </head>
-    <body class="bg_e">
+    <body class="bg_f">
         <!-- main wrapper (without footer) -->    
         <div class="main-wrapper">
 
