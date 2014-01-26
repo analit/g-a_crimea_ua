686f5ec9aa0d7a7029d30f51795ae7c6076da403
Sergei <analit09@mail.ru>
2014-01-26 23:34:14 +0200

    fix send mail

--- wp-content/plugins/ga-contact-form/ga-contact-form.php
+++ wp-content/plugins/ga-contact-form/ga-contact-form.php
@@ -15,14 +15,23 @@ set_include_path(realpath(dirname(__FILE__) . '/../../../library') . PATH_SEPARA
 require_once 'ContactForm.php';
 require_once 'Zend/Controller/Action/Helper/FlashMessenger.php';
 require_once 'Zend/Mail.php';
+require_once 'Zend/Mail/Transport/Smtp.php';
 
 $flash = new Zend_Controller_Action_Helper_FlashMessenger();
 
 $form = new ContactForm();
 
 if (count($_POST) && $form->isValid($_POST)) {
-    sendMessage($_POST);
-    $flash->addMessage("<strong>Ваше сообщение отправлено!</strong><br/>В ближайщее время мы с вами свяжемся ...");
+    try {
+        sendMessage($_POST);
+    } catch (Exception $exc) {
+        $message = "<pre>Ошибка при отправке почты!</pre>";
+        $message .= "<pre>" . $exc->getMessage() . " \n\n" . $exc->getTraceAsString() . "</pre>";
+    }
+
+    $message = isset($message) ? $message : "<strong>Ваше сообщение отправлено!</strong><br/>В ближайщее время мы с вами свяжемся ...";
+
+    $flash->addMessage($message);
     $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header("Location: $url");
     exit();
@@ -30,12 +39,25 @@ if (count($_POST) && $form->isValid($_POST)) {
 
 function sendMessage($data)
 {
-    $mail = new Zend_Mail();
-    $mail->setBodyText($data['body']);
-    $mail->setFrom($data['email'], $data['myname']);
+    $config = array(
+        'auth' => 'login',
+        'username' => 'sergei.garyaga@yandex.ua',
+        'password' => '285285285',
+        'port' => 25
+    );
+
+    $transport = new Zend_Mail_Transport_Smtp('smtp.yandex.ru', $config);
+    
+    $message = "User name: " .$data['myname']."\n";
+    $message .= "User e-mail: " .$data['email']."\n";
+    $message .= "User message: " .$data['body']."\n";
+
+    $mail = new Zend_Mail('utf8');
+    $mail->setBodyText($message);
+    $mail->setFrom('sergei.garyaga@yandex.ua', 'g-a.crimea.ua');
     $mail->addTo('analit09@mail.ru');
-    $mail->setSubject('Message from g-a.crimea.ua');
-    $mail->send();
+    $mail->setSubject($data['subject']);
+    $mail->send($transport);
 }
 
 add_shortcode('contact-form', function () use ($flash, $form) {
